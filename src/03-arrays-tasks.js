6cbfd564d6940d454fb7b6e5ae49df3f935d03da
Onberfrest <62020954+Onberfrest@users.noreply.github.com>
2020-12-14 22:34:33 +0300

    feat: getElementByIndexes task

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -553,8 +553,13 @@ function selectMany(arr, childrenSelector) {
  *   ['one','two','three'], [2]       => 'three'  (arr[2])
  *   [[[ 1, 2, 3]]], [ 0, 0, 1 ]      => 2        (arr[0][0][1])
  */
-function getElementByIndexes(/* arr, indexes */) {
-  throw new Error('Not implemented');
+function getElementByIndexes(arr, indexes) {
+  let output = [].concat(arr);
+  indexes.map((e) => {
+    output = output[e];
+    return e;
+  });
+  return output;
 }
 
 /**
