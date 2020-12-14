58f67998a4e42136199c38eb684d50f02b315a9a
Onberfrest <62020954+Onberfrest@users.noreply.github.com>
2020-12-14 22:38:13 +0300

    feat: swapHeadAndTail task

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -580,8 +580,11 @@ function getElementByIndexes(arr, indexes) {
  *   [ 1, 2, 3, 4, 5, 6, 7, 8 ]   =>  [ 5, 6, 7, 8, 1, 2, 3, 4 ]
  *
  */
-function swapHeadAndTail(/* arr */) {
-  throw new Error('Not implemented');
+function swapHeadAndTail(arr) {
+  const half = Math.floor(arr.length / 2);
+  const head = arr.splice(0, half);
+  const tail = arr.splice(-half);
+  return [...tail, ...arr, ...head];
 }
 
 module.exports = {
