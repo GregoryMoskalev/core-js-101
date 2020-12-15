740c4f66117519712cdc5f02cfa9b8ab83cdf800
Onberfrest <62020954+Onberfrest@users.noreply.github.com>
2020-12-15 14:57:36 +0300

    feat: sortCitiesArray task

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -427,8 +427,19 @@ function toStringList(arr) {
  *      { country: 'Russia',  city: 'Saint Petersburg' }
  *    ]
  */
-function sortCitiesArray(/* arr */) {
-  throw new Error('Not implemented');
+function sortCitiesArray(arr) {
+  return arr.sort((a, b) => {
+    if (b.country > a.country) {
+      return -1;
+    } if (b.country < a.country) {
+      return 1;
+    }
+    if (b.city > a.city) {
+      return -1;
+    }
+
+    return 1;
+  });
 }
 
 /**
